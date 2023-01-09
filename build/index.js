29b807d204e921a87e68a44a6d7d555e71c72a38
mbiga <mbigaram.mb@gmail.com>
2023-01-09 15:41:33 -0300

    api e express

--- build/index.js
+++ build/index.js
@@ -1,7 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const types_1 = require("./types");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003");
+});
+app.get('/ping', (req, res) => {
+    res.send('Pong!');
+});
 console.table((0, database_1.createUser)("u003", "beltrano@email.com", "beltrano99"));
 console.table((0, database_1.getAllUsers)());
 console.table((0, database_1.createProduct)("p004", "Monitor HD", 800, types_1.PRODUCT_CATEGORY.ELECTRONICS));
@@ -10,4 +24,49 @@ console.table((0, database_1.getProductById)("p1"));
 console.table((0, database_1.queryProductsByName)("produto2"));
 console.table((0, database_1.createPurchase)("u003", "p004", 2, 1600));
 console.table((0, database_1.getAllPurchasesFromUserId)("u1"));
+app.get('/users', (req, res) => {
+    res.status(200).send(database_1.users);
+});
+app.get('/products', (req, res) => {
+    res.status(200).send(database_1.products);
+});
+app.get('/products/search', (req, res) => {
+    const q = req.query.q;
+    const result = database_1.products.filter((product) => {
+        return product.name.toLowerCase().includes(q.toLowerCase());
+    });
+    res.status(200).send(result);
+});
+app.post('/users', (req, res) => {
+    const { id, email, password } = req.body;
+    const newUser = {
+        id,
+        email,
+        password
+    };
+    database_1.users.push(newUser);
+    res.status(201).send('Usuario registrado com sucesso!');
+});
+app.post('/products', (req, res) => {
+    const { id, name, price, category } = req.body;
+    const newProduct = {
+        id,
+        name,
+        price,
+        category
+    };
+    database_1.products.push(newProduct);
+    res.status(201).send('Produto registrado com sucesso!');
+});
+app.post('/purchase', (req, res) => {
+    const { userId, productId, quantity, totalPrice } = req.body;
+    const newPurchase = {
+        userId,
+        productId,
+        quantity,
+        totalPrice,
+    };
+    database_1.purchase.push(newPurchase);
+    res.status(201).send('Compra registrada com sucesso!');
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,27 @@ import {
      getProductById,
      queryProductsByName,
      createPurchase,
-     getAllPurchasesFromUserId
+     getAllPurchasesFromUserId     
     } from "./database"
+    import { TUser, TProduto, TPurchase } from "./types"
+import express, { Request, Response} from 'express'  
+import cors from 'cors';  
 import { PRODUCT_CATEGORY } from "./types"
+
+//configuração de servidor node com express
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//exercicio 1 API e Express
+
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003")
+})
+
+app.get('/ping', (req: Request, res: Response) => {
+    res.send('Pong!')
+})
 
 // console.table(users)
 // console.table(products)
@@ -28,11 +46,85 @@ console.table(queryProductsByName("produto2"))
 console.table(createPurchase("u003", "p004", 2, 1600))
 console.table(getAllPurchasesFromUserId("u1"))
 
-// console.table(getAllUsers(users, "u1"))
-// console.table(getAllUsers())
 
-// console.table(getAllProducts())
+app.get('/users', (req: Request, res: Response)=>{
+    res.status(200).send(users)
+})
 
-// console.log(getProductById("p1"))
+app.get('/products', (req: Request, res: Response)=>{
+    res.status(200).send(products)
+})
+
+//Exercicio 2 API e Express
+
+app.get('/products/search', (req: Request, res: Response) =>{
+    const q = req.query.q as string
+
+    const result = products.filter((product)=>{
+       return product.name.toLowerCase().includes(q.toLowerCase())
+    })
+
+    res.status(200).send(result)
+})
+
+//Exercicio 3 API e express
+
+//Criar usuario
+
+app.post('/users', (req: Request, res: Response) => {
+
+    const {id, email, password} = req.body as TUser
+
+    const newUser = {
+        id,
+        email,
+        password
+    }
+
+   users.push(newUser)
+
+    res.status(201).send('Usuario registrado com sucesso!')
+})
+
+//Criar produtos
+
+app.post('/products', (req: Request, res: Response) => {
+
+    const {id, name, price, category} = req.body as TProduto
+
+    const newProduct = {
+        id,
+        name,
+        price,
+        category
+    }
+
+   products.push(newProduct)
+
+    res.status(201).send('Produto registrado com sucesso!')
+})
+
+////Criar purchase
+
+app.post('/purchase', (req: Request, res: Response) => {
+
+    const {userId, productId, quantity, totalPrice} = req.body as TPurchase
+
+    const newPurchase = {
+        userId,
+        productId,
+        quantity,
+        totalPrice,
+    }
+
+   purchase.push(newPurchase)
+
+    res.status(201).send('Compra registrada com sucesso!')
+})
+
+
+
+
+
 
 
